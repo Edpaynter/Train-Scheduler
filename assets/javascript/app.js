9b13a776910abfb1fbaca8043d43d43f27ddc06c
EP <edisonxpaynter@gmail.com>
2019-03-21 17:58:44 -0400

    created assets directory and added javascript file, created html page as well

---
+++ assets/javascript/app.js
@@ -0,0 +1,58 @@
+
+
+
+var config = {
+    apiKey: "AIzaSyDMCfPqegqqjx6XHQ2iT9qSuK07K5DyR2E",
+    authDomain: "train-schedule-fed68.firebaseapp.com",
+    databaseURL: "https://train-schedule-fed68.firebaseio.com",
+    projectId: "train-schedule-fed68",
+    storageBucket: "train-schedule-fed68.appspot.com",
+    messagingSenderId: "129361728383"
+  };
+  firebase.initializeApp(config);
+
+  database = firebase.database()
+
+  $("#submit-train").on("click", function(event){
+    event.preventDefault();
+
+    let newTrain = $("#form-train-name").val().trim()
+    let newDest = $("#form-destination").val().trim()
+    let newTime = $("#form-train-time").val().trim()
+    let newFreq = $("#form-frequency").val().trim()
+
+    newEntry = {
+        train : newTrain,
+        destination : newDest,
+        time : newTime,
+        frequency : newFreq
+    }
+
+    database.ref().push(newEntry)
+
+    $("#form-train-name").val("");
+    $("#form-destination").val("");
+    $("#form-train-time").val("");
+    $("#form-frequency").val("");   
+
+  })
+
+  database.ref().on("child_added", function(snapshot){
+      let trainName = snapshot.val().train
+      let trainDest = snapshot.val().destination
+      let trainTime = snapshot.val().time
+      let trainFreq = snapshot.val().frequency
+
+    
+      let addRow = $("<tr>").append(
+          $("<td>").text(trainName),
+          $("<td>").text(trainDest),
+          $("<td>").text(trainTime),
+          $("<td>").text(trainFreq)
+      )
+    
+      $("tbody").append(addRow)
+
+
+
+    })
